353e2504e27cd2424e195bfad1361dd1715273cc
Chetane <singhchetan0542@gmail.comm>
2021-08-04 11:58:48 +0530

    Changes

---
+++ auth.js
@@ -0,0 +1,16 @@
+// const passport = require('passport');
+// const GoogleStrategy = require('passport-google-oauth2').OAuth2Strategy;
+
+// passport.use(new GoogleStrategy({
+//     authorizationURL: 'https://www.example.com/oauth2/authorize',
+//     tokenURL: 'https://www.example.com/oauth2/token',
+//     clientID: EXAMPLE_CLIENT_ID,
+//     clientSecret: EXAMPLE_CLIENT_SECRET,
+//     callbackURL: "http://localhost:3000/auth/example/callback"
+//   },
+//   function(accessToken, refreshToken, profile, cb) {
+//     User.findOrCreate({ exampleId: profile.id }, function (err, user) {
+//       return cb(err, user);
+//     });
+//   }
+// ));
--- server.js
+++ server.js
@@ -1,105 +1,82 @@
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config()
+  require('dotenv').config()
 }
 
-const express = require('express');
+const express = require('express')
 const app = express()
-const bycrypt = require('bcryptjs');
-const passport = require('passport');
-const flash = require('express-flash'); // To help the user with flash messages
-const session = require('express-session'); // To store the user's session
-const methodOverride = require('method-override');
-const jwt = require('jsonwebtoken');
+const bcrypt = require('bcryptjs')
+const passport = require('passport')
+const flash = require('express-flash')
+const session = require('express-session')
+const methodOverride = require('method-override')
 
 const initializePassport = require('./passport-config')
 initializePassport(
   passport,
   email => users.find(user => user.email === email),
   id => users.find(user => user.id === id)
-)//Finding User based on email
+)
 
 const users = []
 
-app.use(express.json())
-
-app.set('view engine', 'ejs' )
+app.set('view-engine', 'ejs')
 app.use(express.urlencoded({ extended: false }))
-app.use (flash());
-app.use(session({     // session secret
+app.use(flash())
+app.use(session({
   secret: process.env.SESSION_SECRET,
   resave: false,
-  saveUninitialized: false 
-})),
-
-app.use(passport.initialize());
-app.use(passport.session());
-app.use(methodOverride('_method'));
+  saveUninitialized: false
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(methodOverride('_method'))
 
 app.get('/', checkAuthenticated, (req, res) => {
-  res.render('index.ejs', { name: req.user.name})
-  res.send
+  res.render('index.ejs', { name: req.user.name })
 })
 
 app.get('/login', checkNotAuthenticated, (req, res) => {
   res.render('login.ejs')
 })
 
-app.post('/login', checkNotAuthenticated, passport.authenticate('local',
- { // Logging in and redirecting to home page and if failure redirect to login page
+app.post('/login', checkNotAuthenticated, passport.authenticate('local', {
   successRedirect: '/',
   failureRedirect: '/login',
   failureFlash: true
- 
-}));
-// const user = ({
-//   name: req.body.name,
-//   email: req.body.email,
-//   password: hashedPassword
-// })
-// const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
-// res.json({accessToken: accessToken}))) //Will show a message if login fails from the passport-config js file,to be precise from the try catch block of  the function named "initialize"
+}))
 
-
-app.get('/register', checkNotAuthenticated,(req, res) => {
+app.get('/register', checkNotAuthenticated, (req, res) => {
   res.render('register.ejs')
 })
 
-app.post ('/register',checkNotAuthenticated, async (req, res) => {
-try {
-  const hashedPassword = await bycrypt.hashSync(req.body.password, 10)
- 
-  users .push({
-    id: Date.now().toString(),
-    name: req.body.name,
-    email: req.body.email,
-    password: hashedPassword
-  })
-  res.redirect('/login')
-  res.json(users)
-
-  
-} catch  {
-res.redirect('/register')  
-}
-console.log(users)
-
-  
+app.post('/register', checkNotAuthenticated, async (req, res) => {
+  try {
+    const hashedPassword = await bcrypt.hash(req.body.password, 10)
+    users.push({
+      id: Date.now().toString(),
+      name: req.body.name,
+      email: req.body.email,
+      password: hashedPassword
+    })
+    res.redirect('/login')
+  } catch {
+    res.redirect('/register')
+  }
 })
-
-
 
 app.delete('/logout', (req, res) => {
-  req.logout()
+  req.logOut()
   res.redirect('/login')
 })
 
-
-function checkAuthenticated(req, res, next) { //Authentication Check
+function checkAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return next()
   }
+
   res.redirect('/login')
 }
+
 function checkNotAuthenticated(req, res, next) {
   if (req.isAuthenticated()) {
     return res.redirect('/')
@@ -107,8 +84,6 @@ function checkNotAuthenticated(req, res, next) {
   next()
 }
 
-app.listen(3000, () => {
-  console.log("Server is running on port 3000");
-});  
-
-//With passport we don't need to use the usual midleware for login
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server started on port 3000')
+})
